c943db495e1b16477aefd4e2c6aaadf6d8cac44a
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-12 01:33:43 +0000

    adding places_reviews

---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+""" API redirections """
+from models.review import Review
+from werkzeug.exceptions import BadRequest
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+
+
+# Status
+@app_views.route('/reviews/status')
+def reviews_status():
+    """ returns status OK if app is working """
+    return jsonify({"Status": "OK"})
+
+
+# All
+@app_views.route('/reviews', strict_slashes=False)
+def reviews_all():
+    """ retrieves a list of all reviews objects """
+    ret_list = []
+    for obj in storage.all(Review).values():
+        ret_list.append(obj.to_dict())
+    return jsonify(ret_list)
+
+
+# Get by id
+@app_views.route('/reviews/<review_id>',
+                 methods=['GET'],
+                 strict_slashes=False)
+def reviews_get(review_id):
+    """ retrieves a specific review object based on id """
+    obj = storage.get(Review, review_id)
+    if (obj):
+        return jsonify(obj.to_dict())
+    else:
+        abort(404)
+
+
+# Delete by id
+@app_views.route('/reviews/<review_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def reviews_del(review_id):
+    """ delete linked review object """
+    obj = storage.get(Review, review_id)
+    if obj is not None:
+        storage.delete(obj)
+        storage.save()
+        return {}
+    else:
+        abort(404)
+
+
+# Create new
+@app_views.route('/reviews/',
+                 methods=['POST'],
+                 strict_slashes=False)
+def reviews_new():
+    try:
+        obj_JSON = request.get_json()
+        new_obj = Review(**obj_JSON)
+        if not obj_JSON.get('name'):
+            abort(400, description="Missing name")
+    except BadRequest:
+        abort(400, description="Not a JSON")
+
+    storage.new(new_obj)
+    storage.save()
+    return jsonify(storage.get(new_obj.__class__, new_obj.id).to_dict()), 201
+
+
+# Update
+@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
+def reviews_put(review_id):
+    """ Handles PUT request. Updates a Review obj with status 200, else 400 """
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    obj = storage.get(Review, review_id)
+    attrs = request.get_json(force=True, silent=True)
+    if not obj:
+        abort(404)
+    if attrs is None:
+        abort(400, "Not a JSON")
+    else:
+        for key, value in attrs.items():
+            if key in ignore_keys:
+                continue
+            setattr(obj, key, value)
+        storage.save()
+        return jsonify(obj.to_dict()), 200
