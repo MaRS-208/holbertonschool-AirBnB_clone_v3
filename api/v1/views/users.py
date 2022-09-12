21e850425f4feef7c1c59338302f7080c51e08bf
MaRS-208 <manuela.riossosa@gmail.com>
2022-09-12 02:03:45 +0000

    10. User

---
+++ api/v1/views/users.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""Create a new view for User object that\
+handles all default RESTFul API actions"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.user import User
+from werkzeug.exceptions import BadRequest
+
+
+# Status
+@app_views.route('/users/status')
+def users_status():
+    """ returns status OK if app is working """
+    return jsonify({"Status": "OK"})
+
+
+# All
+@app_views.route('/users', strict_slashes=False)
+def users_all():
+    """ retrieves a list of all states objects """
+    ret_list = []
+    for obj in storage.all(User).values():
+        ret_list.append(obj.to_dict())
+    return jsonify(ret_list)
+
+
+# Get by id
+@app_views.route('/users/<city_id>',
+                 methods=['GET'],
+                 strict_slashes=False)
+def users_get(user_id):
+    """ retrieves a specific state object based on id """
+    obj = storage.get(User, user_id)
+    if (obj):
+        return jsonify(obj.to_dict())
+    else:
+        abort(404)
+
+
+# Delete by id
+@app_views.route('/users/<city_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def users_del(user_id):
+    """ delete linked state object """
+    obj = storage.get(User, user_id)
+    if obj is not None:
+        storage.delete(obj)
+        storage.save()
+        return {}
+    else:
+        abort(404)
+
+
+# Create new
+@app_views.route('/users/',
+                 methods=['POST'],
+                 strict_slashes=False)
+def users_new():
+    try:
+        obj_JSON = request.get_json()
+        new_obj = User(**obj_JSON)
+        if not obj_JSON.get('name'):
+            abort(400, description="Missing name")
+    except BadRequest:
+        abort(400, description="Not a JSON")
+
+    storage.new(new_obj)
+    storage.save()
+    return jsonify(storage.get(new_obj.__class__, new_obj.id).to_dict())
+
+
+# Update
+@app_views.route('/users/<city_id>', methods=['PUT'], strict_slashes=False)
+def users_put(city_id):
+    """ Handles PUT request. Updates a State obj with status 200, else 400 """
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    obj = storage.get(User, user_id)
+    attrs = request.get_json(force=True, silent=True)
+    if not obj:
+        abort(404)
+    if attrs is None:
+        abort(400, "Not a JSON")
+    else:
+        for key, value in attrs.items():
+            if key in ignore_keys:
+                continue
+            setattr(obj, key, value)
+        storage.save()
+        return jsonify(obj.to_dict()), 200
