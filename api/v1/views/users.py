8f2468eef2dac7ad88a619db1e6eb65749d14fd2
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-12 15:39:47 +0000

    users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -80,7 +80,7 @@ def users_new():
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def users_put(city_id):
     """ Handles PUT request. Updates a State obj with status 200, else 400 """
-    ignore_keys = ['id', 'created_at', 'updated_at']
+    ignore_keys = ['id', 'email',  'created_at', 'updated_at']
     obj = storage.get(User, user_id)
     attrs = request.get_json(force=True, silent=True)
     if not obj:
